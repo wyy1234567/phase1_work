c0b8f75970b20811508af66ed60f5e4c66ac93f0
Yueying <wangyy395@gmail.com>
2020-11-18 13:59:08 -0500

    create models

--- restaurants/config/routes.rb
+++ restaurants/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :menus
+  resources :restaurants
+  resources :cuisines
+  resources :cities
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
