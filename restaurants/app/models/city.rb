75ea16390bf5b7554c044f45e9d5d9e6f01df197
Yueying <wangyy395@gmail.com>
2020-11-23 18:07:00 -0500

    parse cuisines response

--- restaurants/app/models/city.rb
+++ restaurants/app/models/city.rb
@@ -7,17 +7,17 @@ class City < ApplicationRecord
     def self.fetch_info(api_key, city_name)
         #return a hash, has city infomation and cuisines in that city
         result = {}
-        uri = URI.parse("https://developers.zomato.com/api/v2.1/cities?q=#{city_name}&count=1")
-        request = Net::HTTP::Get.new(uri)
-        request["Accept"] = "application/json"
-        request["user-key"] = api_key
-        req_options = {
-            use_ssl: uri.scheme == "https",
+        city_url = URI.parse("https://developers.zomato.com/api/v2.1/cities?q=#{city_name}&count=1")
+        city_request = Net::HTTP::Get.new(city_url)
+        city_request["Accept"] = "application/json"
+        city_request["user-key"] = api_key
+        city_req_options = {
+            use_ssl: city_url.scheme == "https",
         }
-        response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-            http.request(request)
+        city_response = Net::HTTP.start(city_url.hostname, city_url.port, city_req_options) do |http|
+            http.request(city_request)
         end
-        city_info = JSON.parse(response.body)
+        city_info = JSON.parse(city_response.body)
         city_id = city_info["location_suggestions"][0]["id"]
         result = {:city_info => city_info["location_suggestions"][0]}
         
@@ -26,14 +26,18 @@ class City < ApplicationRecord
         cuisine_request["Accept"] = "application/json"
         cuisine_request["user-key"] = api_key
         cuisine_req_options = {
-            use_ssl: uri.scheme == "https",
+            use_ssl: cuisine_url.scheme == "https",
         }
         cuisine_response = Net::HTTP.start(cuisine_url.hostname, cuisine_url.port, cuisine_req_options) do |http|
             http.request(cuisine_request)
         end
         cuisines = JSON.parse(cuisine_response.body)
-        # cuisines
-        result.merge!({:cuisines => cuisines["cuisines"]})
+        
+        cuisine_list = []
+        cuisines["cuisines"].each do |hash|
+            cuisine_list << hash["cuisine"]
+        end
+        result.merge!({:cuisines => cuisine_list})
         result
     end
 end
