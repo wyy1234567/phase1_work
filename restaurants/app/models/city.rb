28716c2df060b9f227206cd916551ca2efe0e5e5
Yueying <wangyy395@gmail.com>
2020-11-23 17:08:44 -0500

    fetch city info

--- restaurants/app/models/city.rb
+++ restaurants/app/models/city.rb
@@ -1,2 +1,22 @@
 class City < ApplicationRecord
+    has_many :cuisines
+    
+    
+    # search_result = City.fetch_info(api_key, city_name) in controller
+    # https://developers.zomato.com/api/v2.1/cities?q=west%20new%20york&count=1
+    def self.fetch_info(api_key, city_name)
+
+        uri = URI.parse("https://developers.zomato.com/api/v2.1/cities?q=#{city_name}&count=1")
+        request = Net::HTTP::Get.new(uri)
+        request["Accept"] = "application/json"
+        request["user-key"] = api_key
+        req_options = {
+            use_ssl: uri.scheme == "https",
+        }
+        response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+            http.request(request)
+        end
+        hash = JSON.parse(response.body)
+        hash
+    end
 end
--- restaurants/config/routes.rb
+++ restaurants/config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   resources :cuisines
   resources :cities
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/city_and_cuisines/:city_name/:api_key", to: "cities#search"
 end
