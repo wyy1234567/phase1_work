606205d0a9a14c5854cccb191378ea0acd951c32
Yueying <wangyy395@gmail.com>
2020-11-27 17:33:57 -0500

    api request problem

--- restaurants/app/models/review.rb
+++ restaurants/app/models/review.rb
@@ -3,7 +3,8 @@ class Review < ApplicationRecord
     # reviews = Review.search_reviews(cuisine_id, city_id, api_key)
     # https://developers.zomato.com/api/v2.1/search?entity_id=280&entity_type=city&count=5&cuisines=1
     def self.search_reviews(cuisine_id, city_id, api_key)
-        search_url = URI.parse("https://developers.zomato.com/api/v2.1/search?entity_id=#{city_id}&entity_type=city&count=5&cuisines=#{cuisine_id}")
+        result = []
+        search_url = URI.parse("https://developers.zomato.com/api/v2.1/search?entity_id=#{city_id}&entity_type=city&count=3&cuisines=#{cuisine_id}")
         request = Net::HTTP::Get.new(search_url)
         request["Accept"] = "application/json"
         request["user-key"] = api_key
@@ -13,7 +14,34 @@ class Review < ApplicationRecord
         response = Net::HTTP.start(search_url.hostname, search_url.port, req_options) do |http|
             http.request(request)
         end
+        restrurants = JSON.parse(response.body)["restaurants"]
 
-        result = JSON.parse(response.body)
+        restrurants.each do |res|
+            res_and_reviews = {}
+            res_id = res["restaurant"]["R"]["res_id"]
+            res_and_reviews = {:restaurant_info => res["restaurant"]}
+            reviews = Review.fetch_reviews(res_id, api_key)
+            res_and_reviews.merge!({:reviews => reviews})
+            result << res_and_reviews
+
+        end
+        result
     end
+
+
+    def self.fetch_reviews(restrurant_id, api_key)
+        reviews_url = "https://developers.zomato.com/api/v2.1/reviews?res_id=#{restrurant_id}"
+        request = Net::HTTP::Get.new(reviews_url)
+        request["Accept"] = "application/json"
+        request["user-key"] = api_key
+        req_options = {
+            use_ssl: reviews_url.scheme == "https",
+        }
+        response = Net::HTTP.start(reviews_url.hostname, reviews_url.port, req_options) do |http|
+            http.request(request)
+        end
+        result = JSON.parse(response.body)
+        reviews = result["user_reviews"]
+    end
+
 end
