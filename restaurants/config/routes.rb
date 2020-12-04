189561f28dda70db4bb493fa56cce49657b64424
Yueying <wangyy395@gmail.com>
2020-12-04 17:36:30 -0500

    modify routes

--- restaurants/config/routes.rb
+++ restaurants/config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  resources :reviews
-  resources :menus
-  resources :restaurants
-  resources :cuisines
-  resources :cities
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/city_and_cuisines/:city_name/:api_key", to: "cities#search"
   get "/reviews/:cuisine_id/:city_id/:api_key", to: "reviews#find_reviews"
