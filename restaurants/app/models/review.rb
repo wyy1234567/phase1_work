d36ae9b1f9ecb311dd8682f3a9b15126a45d5f33
Yueying <wangyy395@gmail.com>
2020-12-13 22:42:40 -0500

    add error message for reviews

--- restaurants/app/models/review.rb
+++ restaurants/app/models/review.rb
@@ -5,6 +5,7 @@ class Review < ApplicationRecord
     # reviews = Review.search_reviews(cuisine_id, city_id, api_key)
     def self.search_reviews(cuisine_id, city_id, api_key)
         result = []
+        invalid_key_message = "Please enter a valid Zomato API key"
         search_url = URI.parse("https://developers.zomato.com/api/v2.1/search?entity_id=#{city_id}&entity_type=city&count=3&cuisines=#{cuisine_id}")
         request = Net::HTTP::Get.new(search_url)
         request["Accept"] = "application/json"
@@ -15,17 +16,22 @@ class Review < ApplicationRecord
         response = Net::HTTP.start(search_url.hostname, search_url.port, req_options) do |http|
             http.request(request)
         end
-        restrurants = JSON.parse(response.body)["restaurants"]
 
-        restrurants.each do |res|
+        restaurants = JSON.parse(response.body)
+
+        if restaurants["code"] == 403
+            return invalid_key_message
+        end
+
+        restaurants["restaurants"].each do |res|
             res_and_reviews = {}
             res_id = res["restaurant"]["R"]["res_id"]
             res_and_reviews = {:restaurant_info => res["restaurant"]}
             reviews = Review.fetch_reviews(res_id, api_key)
             res_and_reviews.merge!({:reviews => reviews})
             result << res_and_reviews
-
         end
+        
         result
     end
 
